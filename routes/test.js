457e4d9305c9c1ef40647a61fb7d3d4d8872f91d
hardi <hardik.munjaal@gmail.com>
2015-12-28 16:16:07 +0530

    added test files

---
+++ routes/test.js
@@ -0,0 +1,272 @@
+	/*var triple = function(x){
+	     return x*3
+	      }
+
+    console.log(triple(30));
+
+	var animals = [
+	{name : 'atul' , quality : 'chutiyapanti'},
+	{name : 'jaijan' , quality : 'extrakhushi'}
+	];
+
+	var chutiya = animals.filter(function(animal) {
+		return animal.quality === 'chutiyapanti'
+	});*/
+
+	function f1() {
+		// f2(2, 3, function(value) {
+		// 	f3(value);
+		// 	console.log('pehle ye ');
+		// });
+
+		var x =f2(2,3);
+		f3(x);
+	}
+
+	function f2 (a,b) {
+
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+var x,y,chr;
+for(x=1; x<=6; x++)
+{
+   for (y=1; y<x; y++)
+     {
+    chr=chr+("*");        
+      }
+ console.log(chr);
+ chr='';    
+}
+
+
+		var temp = a+b;
+		//callback(temp);
+		//setTimeout(function(){ alert("Hello"); }, 3000);
+		return temp;
+	}
+
+	function f3(val) {
+		var output = val * val;
+		console.log(output);
+	}
+
+
+
+	f1();
+
+
+
+
+
+function subtract(a,b,callback){
+
+	var c= a-b;
+	callback(c);
+}
+
+function print(subtractedValue){
+	var ss = subtractedValue*6;
+}
+
+function x(print){
+	console.log(print);
+}
+
+subtract(5,1,print);
+
+
+
+
+
+function add(a,b)
+{
+
+	var c = a*b/a + a +b + a/b;
+	return c;
+}
+
+function multiply(c)
+{
+	console.log(c*c);
+}
+
+function both()
+{
+	var a = add(3,4);
+	console.log('a:'+a);
+	multiply(a);
+}
+
+
+both();
+
+
+
+function getStudentMarks()
+{
+	var StMarks = [{name : 'hardik' , marks : '23' , gender : 'male'},{name : 'ayushi' , marks : '21' , gender : 'female'},{name : 'shivika' , marks : '19' , gender : 'female'}];
+    return StMarks;
+}
+
+function getTopper(arrayMarks)
+{
+	arrayMarks.forEach()
+}

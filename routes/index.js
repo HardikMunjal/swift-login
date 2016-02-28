418a27d0cd35c7e0d93ba2b1f59f570897ccceca
hardi <hardik.munjaal@gmail.com>
2016-02-28 18:43:12 +0530

    add api to be contruct

--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,45 @@ var userInfo = require('./userInfo');
 module.exports = function (app) {
 
 
- app.get('/_status', function (req, res) {
-			    res.write('OK');
-			    res.status(200).send();
-			  });
+
+
+//Api's created for 
  app.get('/get/teams', team.get);
  app.get('/get/users', team.getUser);  
  app.post('/create/user', team.createUser);
 
  //Live APIS
  app.post('/v1/login', userInfo.validateLoginDetails, userInfo.getDetailsViaUserid,error);
+
+
  app.get('/v1/validate/email',userInfo.validateExistenceOfEmail,error);
  app.get('/v1/validate/mobile',userInfo.validateExistenceOfMobile,error);
- app.get('/get/user', userInfo.getRegistrationDetail, userInfo.saveUser);
  app.post('/register/user', userInfo.validate, userInfo.saveUser);
 
+
+ app.get('/get/user', userInfo.getRegistrationDetail, userInfo.saveUser);
+ //app.get('/v1/user/:user_id',);          //if admin then check for query parameter , in case of admin send list of all user
+ //app.post('/v1/user/:user_id',);         //if admin then check for query parameter
+ //app.put('/v1/user',);                   //if admin then check for query parameter
+ //app.delete('/v1/user/:user_id',);       //only for admin
+
+ //app.get('/v1/user/:user_id/friend/',);  //status in query parameter to check for type of friend
+ //app.post('/v1/user/:user_id/friend/:friend_id',);   //to remove friend from your list
+ //app.put('/v1/user/:user_id/friend/:friend_id',);    //to create friend in you list
+
+ //app.get('/v1/user/user:id/group/:group_id/',);  //return the list of users in specific group
+ //app.post('/v1/user/:user_id/friend/:friend_id',);   //to remove friend from your list
+ //app.put('/v1/user/:user_id/group/:group_id',);    //to create group of friend in your list
+
+  //app.get('/v1/user/user:id/group/:group_id/',);  //return the list of users in specific group
+
+
+  //app.get('/v1/group/:group_id');  //return the group details and their user details 
+
+
+ 
+
+  
 
  //after creation i have to move this to frontend application
  app.get('/open/chat', function(req, res, next) {
